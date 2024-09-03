fbb762179cd07a48dd27a4bb4a673382fdf8f795
Kendra Riga <kendra@super.gd>
2024-09-03 10:30:02 -0700

    Fix issue with class variables on Solidus 3.1.x
    This popped up in a few places inside PW, I fixed this inside the
    bundled code in PW and it seemed to work so I am making the change here.

--- app/helpers/api/api_helpers_decorator.rb
+++ app/helpers/api/api_helpers_decorator.rb
@@ -1,3 +1,5 @@
 Spree::Api::ApiHelpers.class_eval do
-  class_variable_set(:@@order_attributes, class_variable_get(:@@order_attributes).push(:bulk_discount_total))
+  mattr_reader :order_attributes
+
+  class_variable_set(:@@order_attributes, class_variable_get(:@@order_attributes)&.push(:bulk_discount_total))
 end
